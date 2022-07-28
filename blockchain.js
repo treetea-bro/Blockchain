d77e6a5713cf7ea1d17d0c7d69aa8a19534ef710
Seungmin lee <sorpwjdafh@naver.com>
2022-07-28 11:56:09 +0900

    자가제한시스템 및 블록 유효성 검증 추가

--- blockchain.js
+++ blockchain.js
@@ -15,10 +15,14 @@ class Blockchain {
   }
 
   // 블록을 블록체인에 추가
-  addBlock(block) {
-    // 유효성검증 추가되어야 함
-    this.blockchain.push(block);
-    console.log("추가된 블록", block);
+  addBlock(newBlock) {
+    // TODO: 유효성검증 추가되어야 함
+    const oldBlock = this.getLastBlock();
+    if (this.isValidBlock(oldBlock, newBlock)) {
+      // TODO: 블록전파
+      this.blockchain.push(newBlock);
+      console.log("추가된 블록", newBlock);
+    }
   }
 
   // 난이도를 활용해서 비트구하기 (난이도 -> 비트 -> 목표값)
@@ -99,11 +103,12 @@ class Blockchain {
     // 해시퍼즐을 찾는 작업
     const target = this.getTarget(newDifficulty);
     while (!(newBlock.getHash() <= target)) {
-      console.log("목표값", target);
-      console.log("논스", newBlock.nonce);
-      console.log("해시값", newBlock.getHash());
+      // console.log("목표값", target);
+      // console.log("논스", newBlock.nonce);
+      // console.log("해시값", newBlock.getHash());
       newBlock.nonce++;
     }
+    console.log("해시 퍼즐 값", target);
     console.log("해시 퍼즐 정답 논스", newBlock.nonce);
     console.log("해시 정답 값", newBlock.getHash());
     newBlock.hash = newBlock.getHash();
@@ -111,8 +116,31 @@ class Blockchain {
     this.addBlock(newBlock);
   }
 
+  // 자가제한시스템
   getDifficulty(difficulty) {
-    // 자가제한시스템
+    // 평균 블록생성시간을 10초에 수렴하도록 하려한다.
+    const avgBlockTime = 10;
+    // 확인 블록 카운트 10개
+    const intervalBlockCount = 10;
+    // 마지막 블록
+    const lastBlock = this.getLastBlock();
+    // 마지막 블록이 제네시스 블록이 아니여야하고, 확인 블록 카운트의 배수여야 한다.
+    if (lastBlock.index > 0 && lastBlock.index % intervalBlockCount === 0) {
+      // 라스트 블록 기준으로 10번째 앞에 있는 블록 생성 시간
+      let prevTime =
+        this.blockchain[this.blockchain.length - intervalBlockCount - 1]
+          .timestamp;
+      // 라스트 블록 생성 시간
+      let lastTime = lastBlock.timestamp;
+      // 평균 시간을 구함
+      let avgTime = (lastTime - prevTime) / intervalBlockCount / 1000;
+      // 구해진 평균 시간과 정의된 평균시간을 비교하여 배수를 정함
+      let multiple = avgTime > avgBlockTime ? 1 / 4 : 4;
+      // 난이도에 반영
+      difficulty *= multiple;
+      console.log("변경된 난이도", difficulty);
+    }
+
     return difficulty;
   }
 
@@ -120,9 +148,35 @@ class Blockchain {
     return this.blockchain[this.blockchain.length - 1];
   }
 
-  // 난이도 조절
+  // 유효성 검증
+  // 1. 블록 유효성 검증
+  isValidBlock(oldBlock, newBlock) {
+    // if (!oldBlock) return true;
+    const result =
+      newBlock.index > 0 &&
+      oldBlock.hash === newBlock.previousHash &&
+      newBlock.getHash() === newBlock.hash;
+    if (!result) console.log("블록이 유효하지 않습니다!");
+
+    return result;
+  }
+  // 2. 체인 유효성 검증
+  isValidBlockchain(blockchain) {
+    let result = true;
+    if (blockchain.length === 1) return true;
+    for (let i = 0; i < blockchain.length - 1; i++)
+      if (!(blockchain[i].hash === blockchain[i + 1].previousHash))
+        return false;
+
+    return result;
+  }
 }
 
 const blockchain = new Blockchain();
 
-const target = blockchain.mining();
+for (let i = 0; i < 10; i++) blockchain.mining();
+
+console.log(
+  "체인 유효성 검증",
+  blockchain.isValidBlockchain(blockchain.blockchain)
+);
